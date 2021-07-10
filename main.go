d823d1d406761a5b3c8241ad7dca303cff1031b7
Khoi Nguyen <94nguyen@cua.edu>
2021-07-11 01:29:34 +0700

    feature/add-gorm
    [add] add gorm to application

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"net/http"
 	"os"
@@ -11,9 +10,14 @@ import (
 	middleware "github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
 	_ "github.com/lib/pq"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
+	// Serve and listen for backend application
+	app := echo.New()
+
 	// Connect with Postgres database
 	db_port, err := strconv.Atoi(os.Getenv("DB_PORT"))
 	if err != nil {
@@ -24,26 +28,39 @@ func main() {
 	db_password := os.Getenv("DB_PASSWORD")
 	db_name := os.Getenv("DB_NAME")
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", db_host, db_port, db_user, db_password, db_name)
-	db, err := sql.Open("postgres", psqlInfo)
+	db, err := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Cannot connect to Postgres database", err)
 	}
-	if err := db.Ping(); err != nil {
+
+	sqlDB, err := db.DB()
+	if err != nil {
 		panic(err)
 	} else {
-		fmt.Println("Postgres DB connected...")
+		if err := sqlDB.Ping(); err != nil {
+			panic(err)
+		} else {
+			fmt.Println("Postgres DB connected...")
+		}
 	}
 
-	// Serve and listen for backend application
-	app := echo.New()
-
 	// Middlewares
-	app.Use(middleware.Logger())
+	app.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "${time_rfc3339_nano} | method: ${method} | endpoint: ${uri} | status: ${status} | agent: ${user_agent}\n",
+	}))
+	// app.Use(middleware.JWTWithConfig(middleware.JWTConfig{
+	// 	SigningKey:  []byte("wqGyEBBfPK9w3Lxw"),
+	// 	TokenLookup: "query:token",
+	// }))
 	app.Use(middleware.Recover())
 
 	// Routes
 	app.GET("/", func(ctx echo.Context) error {
-		return ctx.String(http.StatusOK, "Hello, world!")
+		var res struct {
+			Message string `json:"message"`
+		}
+		res.Message = "Hello, world!"
+		return ctx.JSON(http.StatusOK, res)
 	})
 
 	// Run application
